737136531c185f99cff1f5052fae87030cfd013b
manhal2 <32019086+manhal2@users.noreply.github.com>
2017-09-18 21:06:51 +0200

    DataTypeFloat-Point
    Different Type of DataType Float point

---
+++ DataType-Float/DataType-Float/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType_Float
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MinValue;
+
+            decimal dcMin = decimal.MinValue;
+            decimal dcMax = decimal.MaxValue;
+
+            Console.WriteLine("float Min. Value = {0}    float Max. Value = {1}\n decimal Min. Value = {2}\n decimal Max. Value = {3}", flMin, flMax, dcMin,dcMax);
+            Console.ReadKey();
+        }
+    }
+}
